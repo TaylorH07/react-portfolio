8360fa01ffdfccc70cc8ee5a80a93ddd8dd3881b
taylor.holmes <taylorh2343@gmail.com>
2022-04-10 22:39:55 -0500

    added styles to hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa'
 import Logo from '../assets/myLogo2.png'
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
             <img src={Logo} alt="Logo Image" style={{width: '50px'}} />
         </div>
-        <div>
-            <ul className = 'flex'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Projects</li>
-                <li>Contact</li> 
-            </ul>
-        </div>
 
-        {/* Hamburger */}
-        <div className = 'hidden'>
-            <FaBars />
-        </div>
-
-        {/* Mobile Menu */}
-        <ul className = 'hidden'>
+        {/* Menu */}
+        <ul className='hidden md:flex'>
             <li>Home</li>
             <li>About</li>
             <li>Skills</li>
@@ -32,8 +21,22 @@ const Navbar = () => {
             <li>Contact</li> 
         </ul>
 
+        {/* Hamburger */}
+        <div onClick={handleClick} className='md:hidden z-10'>
+            {!nav ? <FaBars /> : <FaTimes />}
+        </div>
+
+        {/* Mobile Menu */}
+        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+            <li className = 'py-6 text-4xl'>Home</li>
+            <li className = 'py-6 text-4xl'>About</li>
+            <li className = 'py-6 text-4xl'>Skills</li>
+            <li className = 'py-6 text-4xl'>Projects</li>
+            <li className = 'py-6 text-4xl'>Contact</li> 
+        </ul>
+
         {/* Social Icons */}
-        <div></div>
+        <div className = 'hidden'></div>
 
     </div>
   )
