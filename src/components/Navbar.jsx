4c59eb42ae097faa302609eb00561a22c3fe3c32
taylor.holmes <taylorh2343@gmail.com>
2022-04-10 22:03:23 -0500

    added logo navbarcss styles and navbar contents

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import Logo from '../assets/myLogo2.png'
 
 const Navbar = () => {
   return (
-    <div>
-        
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        <div>
+            <img src={Logo} alt="Logo Image" style={{width: '50px'}} />
+        </div>
+        <div>
+            <ul className = 'flex'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contact</li> 
+            </ul>
+        </div>
+
+        {/* Hamburger */}
+        <div className = 'hidden'>
+            <FaBars />
+        </div>
+
+        {/* Mobile Menu */}
+        <ul className = 'hidden'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Projects</li>
+            <li>Contact</li> 
+        </ul>
+
+        {/* Social Icons */}
+        <div></div>
+
     </div>
   )
 }
