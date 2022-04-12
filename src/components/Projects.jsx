041040bd66fdb8d2e305ba1ddcf2dc742034e3ec
taylor.holmes <taylorh2343@gmail.com>
2022-04-11 21:19:21 -0500

    began project section

---
+++ src/components/Projects.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div name='projects' className='w-full md:h-screen text-gray-300 bg-[#040412]' >
+        <div className='max-w' >
+            <div>
+                <p>Projects</p>
+                <p>// Check out some of my recent Projects</p>
+            </div>
+            
+            <div>
+                <div>
+                    <div>
+                        <span>
+
+                        </span>
+                        <div>
+                            <a href="/">
+                                <button></button>
+                            </a>
+                            <a href="/">
+                                <button></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Projects
