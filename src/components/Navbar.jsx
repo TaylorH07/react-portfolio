05325c37fde3057031abf04ee1f530bb36b9c903
taylor.holmes <taylorh2343@gmail.com>
2022-04-17 10:59:39 -0500

    added resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -97,7 +97,7 @@ const Navbar = () => {
                     </a>
                 </li>
                 <li className=' w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-purple-800 '>
-                    <a className='flex justify-between items-center w-full text-gray-300 ' href="/">
+                    <a className='flex justify-between items-center w-full text-gray-300 ' href="https://docs.google.com/document/d/1UVBFZn6hx4ORGUutvimE8Q4dAClsF8eDect_5zEZCl8/edit?usp=sharing">
                         Resume <BsFillPersonLinesFill size={30} />
                     </a>
                 </li>
