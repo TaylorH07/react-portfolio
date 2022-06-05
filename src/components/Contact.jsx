c8a58cb9b618ce5af39edd3d9374c806d333fc03
taylor.holmes <taylorh2343@gmail.com>
2022-06-05 02:09:22 -0500

    adjusted components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <div name='contact' className='w-full h-screen bg-[#040412] flex justify-center items-center p-4 '>
         <form method='POST' action="https://getform.io/f/e3b9b708-de84-483b-bc2f-8900fda3225f" className='flex flex-col max-w-[600px] w-full ' >
             <div className='pb-8 ' >
-                <p className='text-4xl font-bold inline border-b-4 border-violet-600 text-gray-300 ' >Contact</p>
+                <p className='text-4xl font-bold inline border-b-4 border-[#45ffd7] text-gray-300 ' >Contact</p>
                 <p className='text-gray-300 py-4 ' >// Submit the form or send me an email - taylorh2343@gmail.com </p>
             </div>
             <input  className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
