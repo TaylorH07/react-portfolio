d55a42479a38af225558a248a146a4f94da61425
taylor.holmes <taylorh2343@gmail.com>
2022-06-08 22:32:07 -0500

    added anaglyph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
         <div className='flex flex-col justify-center items-center w-full h-full'>
             <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8 '>
                 <div className='sm:text-right pb-8 pl-4 '>
-                    <p className='text-4xl font-bold inline border-b-4 border-[#45ffd7]'>
+                    <p className='text-4xl font-bold inline border-b-4 border-[#45ffd7] anaglyph'>
                         About 
                     </p>
                 </div>
@@ -20,7 +20,6 @@ const About = () => {
                     <p>
                         - I recently graduated from SMU’s Full Stack Web Development Program while teaching students with learning disabilities, and coaching track and football. I bring a strong work ethic, unparalleled patience, and an itch to learn and deliver great applications. With strengths in creativity, teamwork, and building projects from ideation to execution, I’m eager to apply my knowledge, and creatively solve problems to achieve the main goal.
 
-                        
                     </p>
                 </div>
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
     <div name='projects' className='w-full md:h-screen text-gray-300 bg-[#02020c]' >
         <div className=' max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full ' >
             <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#45ffd7] ' >Projects</p>
+                <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#45ffd7] anaglyph ' >Projects</p>
                 <p className='py-6 '>  Check out some of my recent Projects </p>
             </div>
             {/* Grid container */}
