887f3023a1c3f628e3f0ff5d4254dccbb9eda2f1
taylor.holmes <taylorh2343@gmail.com>
2022-06-08 23:08:23 -0500

    added no refferer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -87,7 +87,7 @@ const Navbar = () => {
                     </a>
                 </li>
                 <li className=' w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#3333] '>
-                    <a className='flex justify-between items-center w-full text-gray-300 ' href="https://github.com/TaylorH07" target="_blank">
+                    <a className='flex justify-between items-center w-full text-gray-300 ' href="https://github.com/TaylorH07" rel="noreferrer" target="_blank">
                         Github <FaGithub size={30} />
                     </a>
                 </li>
